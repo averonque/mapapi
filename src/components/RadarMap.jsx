41aa63aced2fe0d17016d3d29e6a606545f1dcb2
unknown <averonque09@gmail.com>
2025-11-07 21:33:48 -0800

    Cjhanges

--- src/components/RadarMap.jsx
+++ src/components/RadarMap.jsx
@@ -7,7 +7,7 @@ export default function RadarMap() {
 
   useEffect(() => {
     const fetchRadar = async () => {
-      const res = await axios.get('https://188.137.240.130:8000/api/rala?min_lat=51&max_lat=54&min_lon=235&max_lon=238'); // Update with your backend URL
+      const res = await axios.get('https://188.137.240.130/api/rala?min_lat=51&max_lat=54&min_lon=235&max_lon=238'); // Update with your backend URL
       setPoints(res.data);
     };
     fetchRadar();
